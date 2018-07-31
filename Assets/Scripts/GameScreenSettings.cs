0f3891557ee8c347812e83853f03bf0e5a536200
DESKTOP-F282G8U\Tim Ritter <timritter@gmx.net>
2018-07-31 17:30:23 +0200

    *wheel

---
+++ Assets/Scripts/GameScreenSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScreenSettings : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+       // Screen.SetResolution(1080,1920, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Screen.orientation == ScreenOrientation.Portrait)
+        {
+            //Screen.SetResolution(1080, 1920, false);
+            // Screen.orientation = ScreenOrientation.Landscape;
+        }
+        else Screen.orientation = ScreenOrientation.Portrait;
+    }
+}
