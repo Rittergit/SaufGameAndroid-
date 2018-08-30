164b4b12e940e307a804b94c1fbd17f12fe0a97c
LAPTOP-PB251RK3\User <frederik.wollny@outlook.de>
2018-08-30 20:57:54 +0200

    *added icons to wheel

--- Assets/Scripts/Game Scripts/RollTurning.cs
+++ Assets/Scripts/Game Scripts/RollTurning.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RollTurning : MonoBehaviour {
 
     public float slow=0.5f,slow2=0.2f,speed;
-    public double startAngel;
+    public float startAngel;
     private int sides=32;
     private double[] angels;
     public string ausgabe;
@@ -18,16 +18,44 @@ public class RollTurning : MonoBehaviour {
     private bool isRotating;
     private double endAngel=0;
 
+    public float x, y, z, al, be, ga;
+
+    public Transform[] sprites;
+    public Transform par;
+
     // Use this for initialization
     void Start() {
+
         rb.GetComponent<Rigidbody>();
         angels = new double[sides];
+        startAngel = rb.transform.eulerAngles.z;
 
         for(int i=0; i<sides; i++)
         {
             angels[i] = startAngel + (double)360 / (double)sides * (double)i;
             Debug.Log(angels[i]);
         }
+
+        for (int i =0; i<sides; i++)
+        {
+            sprites[GameHandler.sideList[i]-1].GetComponent<GameObject>();
+            sprites[GameHandler.sideList[i]-1].name = "hurensohn";
+            Transform zw = Instantiate(sprites[GameHandler.sideList[i]-1], new Vector3(0, 0, 0), Quaternion.identity) ;
+            zw.SetParent(par,false);
+            zw.localScale = new Vector3(0.15f, 0.03f, 1);
+            zw.position = new Vector3(0.5f, 0, -5.35f);
+            zw.eulerAngles = new Vector3(al,be,ga);
+
+            rb.transform.eulerAngles = new Vector3(rb.transform.eulerAngles.x, rb.transform.eulerAngles.y, (float)angels[i] + 360 / (float)sides);
+        }
+        
+
+
+        //spr = ga.AddComponent<SpriteRenderer>();
+        //spr.sprite = icon;
+        
+
+        
 
 	}
 	
